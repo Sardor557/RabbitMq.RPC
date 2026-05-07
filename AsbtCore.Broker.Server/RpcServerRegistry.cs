83e60e6bf984ec9283bf7cd0c41411c4c3decf54
admin <tdav@mail.ru>
2026-05-07 16:48:01 +0500

    refactor(server): build RpcMethodEntry with compiled invoker per interface method

--- AsbtCore.Broker.Server/RpcServerRegistry.cs
+++ AsbtCore.Broker.Server/RpcServerRegistry.cs
@@ -1,70 +1,67 @@
-﻿using AsbtCore.Broker.Core.Abstractions;
+using System.Reflection;
+using AsbtCore.Broker.Core.Abstractions;
 
-namespace AsbtCore.Broker.Server
+namespace AsbtCore.Broker.Server;
+
+public sealed class RpcServerRegistry
 {
-    public sealed class RpcServerRegistry
+    private readonly Dictionary<string, RpcServerDescriptor> map;
+
+    public RpcServerRegistry(
+        IEnumerable<RpcServerRegistration> registrations,
+        IRpcRouteResolver routeResolver)
     {
-        private readonly Dictionary<string, RpcServerDescriptor> map;
+        map = new Dictionary<string, RpcServerDescriptor>(StringComparer.Ordinal);
 
-        public RpcServerRegistry(
-            IEnumerable<RpcServerRegistration> registrations,
-            IRpcRouteResolver routeResolver)
+        foreach (var registration in registrations)
         {
-            map = new Dictionary<string, RpcServerDescriptor>(StringComparer.Ordinal);
+            var interfaceType = registration.InterfaceType;
+            var implementationType = registration.ImplementationType;
 
-            foreach (var registration in registrations)
-            {
-                var interfaceType = registration.InterfaceType;
-                var implementationType = registration.ImplementationType;
+            var methods = BuildMethodMap(interfaceType, implementationType);
+            var route = registration.ExplicitRoute ?? routeResolver.Resolve(interfaceType);
 
-                var methods = BuildMethodMap(interfaceType, implementationType);
-                var route = registration.ExplicitRoute ?? routeResolver.Resolve(interfaceType);
+            var descriptor = new RpcServerDescriptor(
+                interfaceType,
+                implementationType,
+                route,
+                methods);
 
-                var descriptor = new RpcServerDescriptor(
-                    interfaceType,
-                    implementationType,
-                    route,
-                    methods);
-
-                map[descriptor.InterfaceName] = descriptor;
-            }
+            map[descriptor.InterfaceName] = descriptor;
         }
+    }
 
-        public bool TryGet(string interfaceName, out RpcServerDescriptor descriptor)
-        {
-            return map.TryGetValue(interfaceName, out descriptor!);
-        }
+    public bool TryGet(string interfaceName, out RpcServerDescriptor descriptor)
+        => map.TryGetValue(interfaceName, out descriptor!);
 
-        public IReadOnlyCollection<string> GetRoutes()
+    public IReadOnlyCollection<string> GetRoutes()
+        => map.Values
+            .Select(x => x.Route)
+            .Distinct(StringComparer.Ordinal)
+            .ToArray();
+
+    private static Dictionary<string, RpcMethodEntry> BuildMethodMap(Type interfaceType, Type implementationType)
+    {
+        var result = new Dictionary<string, RpcMethodEntry>(StringComparer.Ordinal);
+        var map = implementationType.GetInterfaceMap(interfaceType);
+
+        for (int i = 0; i < map.InterfaceMethods.Length; i++)
         {
-            return map.Values
-                .Select(x => x.Route)
-                .Distinct(StringComparer.Ordinal)
+            var interfaceMethod = map.InterfaceMethods[i];
+            var targetMethod = map.TargetMethods[i];
+
+            var parameterTypeNames = interfaceMethod
+                .GetParameters()
+                .Select(p => p.ParameterType.AssemblyQualifiedName
+                             ?? p.ParameterType.FullName
+                             ?? throw new InvalidOperationException($"Cannot get type name for {p.ParameterType}."))
                 .ToArray();
+
+            var key = RpcServerDescriptor.BuildMethodKey(interfaceMethod.Name, parameterTypeNames);
+            var invoker = RpcServerMethodInvoker.Build(targetMethod);
+            result[key] = new RpcMethodEntry(targetMethod, invoker);
         }
 
-        private static Dictionary<string, System.Reflection.MethodInfo> BuildMethodMap(Type interfaceType, Type implementationType)
-        {
-            var result = new Dictionary<string, System.Reflection.MethodInfo>(StringComparer.Ordinal);
-            var map = implementationType.GetInterfaceMap(interfaceType);
-
-            for (int i = 0; i < map.InterfaceMethods.Length; i++)
-            {
-                var interfaceMethod = map.InterfaceMethods[i];
-                var targetMethod = map.TargetMethods[i];
-
-                var parameterTypeNames = interfaceMethod
-                    .GetParameters()
-                    .Select(p => p.ParameterType.AssemblyQualifiedName
-                                 ?? p.ParameterType.FullName
-                                 ?? throw new InvalidOperationException($"Cannot get type name for {p.ParameterType}."))
-                    .ToArray();
-
-                var key = RpcServerDescriptor.BuildMethodKey(interfaceMethod.Name, parameterTypeNames);
-                result[key] = targetMethod;
-            }
-
-            return result;
-        }
+        return result;
     }
 }
