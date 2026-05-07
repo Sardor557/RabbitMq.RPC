f3afd928e16b002ae5ecf7cb2ba672be9a0b9421
admin <tdav@mail.ru>
2026-05-07 17:22:52 +0500

    feat(benchmarks): add TypeResolutionBench

---
+++ Benchmarks/AsbtCore.Broker.Benchmarks/TypeResolutionBench.cs
@@ -0,0 +1,37 @@
+using AsbtCore.Broker.Core.Serialization;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace AsbtCore.Broker.Benchmarks;
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net90)]
+public class TypeResolutionBench
+{
+    private string aqn = null!;
+
+    [Params(1, 10, 1000)]
+    public int Calls { get; set; }
+
+    [Params(LegacyOrNew.Legacy, LegacyOrNew.New)]
+    public LegacyOrNew Mode { get; set; }
+
+    [GlobalSetup]
+    public void Setup() => aqn = typeof(Guid).AssemblyQualifiedName!;
+
+    [Benchmark]
+    public Type Resolve()
+    {
+        Type t = typeof(object);
+        for (int i = 0; i < Calls; i++)
+        {
+            t = Mode switch
+            {
+                LegacyOrNew.Legacy => Type.GetType(aqn, throwOnError: true)!,
+                LegacyOrNew.New => TypeNameCache.Resolve(aqn),
+                _ => throw new InvalidOperationException()
+            };
+        }
+        return t;
+    }
+}
