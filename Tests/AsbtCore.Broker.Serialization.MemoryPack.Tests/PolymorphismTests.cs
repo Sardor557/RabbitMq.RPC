1d22bcdec052f2ae7dedf97e8c5ef5f39a343b90
admin <tdav@mail.ru>
2026-05-14 12:16:00 +0500

    test(memorypack): cover union duplicate, duplicate tag, and null value

--- Tests/AsbtCore.Broker.Serialization.MemoryPack.Tests/PolymorphismTests.cs
+++ Tests/AsbtCore.Broker.Serialization.MemoryPack.Tests/PolymorphismTests.cs
@@ -4,8 +4,23 @@ using AsbtCore.Broker.Serialization.MemoryPack;
 using AsbtCore.Broker.Serialization.MemoryPack.Polymorphism;
 using AsbtCore.Broker.Serialization.MemoryPack.Tests.Fixtures;
 
+[NotInParallel]
 public sealed class PolymorphismTests
 {
+    [Before(Class)]
+    public static Task ResetUnionRegistrationsBeforeClass()
+    {
+        MemoryPackRpcOptions.ResetRegisteredUnionsForTests();
+        return Task.CompletedTask;
+    }
+
+    [Before(Test)]
+    public Task ResetUnionRegistrations()
+    {
+        MemoryPackRpcOptions.ResetRegisteredUnionsForTests();
+        return Task.CompletedTask;
+    }
+
     [Test]
     public async Task Union_RoundTrips_AsBase()
     {
@@ -20,5 +35,36 @@ public sealed class PolymorphismTests
         await Assert.That(roundtrip).IsTypeOf<Cat>();
         await Assert.That(((Cat)roundtrip!).IsIndoor).IsTrue();
         await Assert.That(roundtrip.Name).IsEqualTo("Mia");
+    }
+
+    [Test]
+    public async Task Union_DuplicateRegistration_Throws()
+    {
+        var options = new MemoryPackRpcOptions().RegisterUnion<AnimalBase>(b => b.Add<Cat>(1));
+        _ = new MemoryPackRpcSerializer(options);
+
+        var options2 = new MemoryPackRpcOptions().RegisterUnion<AnimalBase>(b => b.Add<Cat>(1));
+        await Assert.That(() => new MemoryPackRpcSerializer(options2))
+            .Throws<InvalidOperationException>();
+    }
+
+    [Test]
+    public async Task Union_DuplicateTag_Throws()
+    {
+        var options = new MemoryPackRpcOptions();
+        await Assert.That(() => options.RegisterUnion<AnimalBase>(b => b.Add<Cat>(1).Add<Dog>(1)))
+            .Throws<InvalidOperationException>();
+    }
+
+    [Test]
+    public async Task Union_NullValue_RoundTrips()
+    {
+        var options = new MemoryPackRpcOptions().RegisterUnion<AnimalBase>(b => b.Add<Cat>(1));
+        var serializer = new MemoryPackRpcSerializer(options);
+
+        var bytes = serializer.SerializeFragment(null, typeof(AnimalBase));
+        var roundtrip = serializer.DeserializeFragment(bytes, typeof(AnimalBase));
+
+        await Assert.That(roundtrip).IsNull();
     }
 }
