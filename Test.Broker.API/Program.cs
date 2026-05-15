fe9cecbf25ef645ab3a2676fb076ec6c7a9161ad
admin <tdav@mail.ru>
2026-05-15 11:21:43 +0500

    Broker.API fix

--- Test.Broker.API/Program.cs
+++ Test.Broker.API/Program.cs
@@ -2,79 +2,34 @@ using AsbtCore.Broker.API.Services;
 using AsbtCore.Broker.Demo.Contracts;
 using AsbtCore.Broker.Serialization.MemoryPack;
 using AsbtCore.Broker.Server;
-using Microsoft.AspNetCore.HttpOverrides;
-using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Exceptions;
-using Serilog.Settings.Configuration;
 
 namespace AsbtCore.Broker.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = Host.CreateApplicationBuilder(args);
 
-            builder.Configuration.AddEnvironmentVariables();
-
-            var configurationAssemblies = new[]
+            builder.Services.AddSerilog((services, loggerConfiguration) =>
             {
-                typeof(ConsoleLoggerConfigurationExtensions).Assembly,
-                typeof(FileLoggerConfigurationExtensions).Assembly,
-            };
+                loggerConfiguration
+                    .ReadFrom.Configuration(builder.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext();
+            });
 
-            var options = new ConfigurationReaderOptions(configurationAssemblies);
-
-            Log.Logger = new LoggerConfiguration()
-                    .Enrich.FromLogContext()
-                    .Enrich.WithMachineName()
-                    .Enrich.WithExceptionDetails()
-                    .Enrich.WithProperty("Environment", builder.Environment)
-                    .ReadFrom.Configuration(builder.Configuration, options)
-                    .CreateLogger();
-
-            builder.Services.AddControllers(o => { o.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true; })
-                            .AddNewtonsoftJson(o => { o.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); });              
-
-            builder.Services.ApiMyVersion();
-            builder.Services.AddMySwagger();
-            builder.Services.AddSerilog();
-            builder.Services.AddMyResponseCompression();
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAllHeaders", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyHeader()
-                           .AllowAnyMethod();
-                });
-            }); 
-
-            builder.Services.AddMemoryCache();
-            builder.Services.AddHttpContextAccessor();
-
-            builder.Services.AddRabbitRpcServer(builder.Configuration)
+            builder.Services
+                .AddRabbitRpcServer(builder.Configuration)
                 .UseMemoryPackRpcSerialization()
                 .Register<IUserService, UserService>(ServiceLifetime.Scoped);
 
+            using var host = builder.Build();
 
-            var app = builder.Build();
-            app.UseMySwagger();
-            app.UseMyStaticFiles();
-
-            app.UseRouting();
-            app.UseCors("AllowAllHeaders");
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto });
-            app.MapControllers();
-
-            app.UseSerilogRequestLogging();
-            app.UseResponseCompression();
-
-            app.Run();
+            await host.RunAsync();
         }
     }
 }
